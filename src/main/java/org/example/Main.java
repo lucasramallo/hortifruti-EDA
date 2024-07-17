68e1580f6fe1320b5ec5ce2400fae4e7cf76cbdb
Raykkoner <raykkonerd@gmail.com>
2024-07-17 11:19:42 -0300

    adição de mais de dois produtos

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,26 +6,35 @@ import org.example.DoublyLinkedList.DoublyLinkedList;
 import org.example.util.Sorter;
 
 public class Main {
-    // int menu(){
-    //     Scanner input = new Scanner(System.in);
+    public void init(){
+        int choice = menu();
 
-    //     System.out.println("=== Menu ===");
-    //     System.out.println("1 - Adicionar produto");
-    //     System.out.println("4 - Checar existência de produto");
-    //     System.out.println("5 - Sair");
+        while(choice != 5){
+            choice = menu();
+        }
+    }
+    public int menu(){
+         Scanner input = new Scanner(System.in);
 
-    //     int option = input.nextInt();
-    //     System.out.println();
+         System.out.println("=== Menu ===");
+         System.out.println("1 - Adicionar produto");
+         System.out.println("4 - Checar existência de produto");
+         System.out.println("5 - Sair");
 
-    //     return option;
-    // }
+         int option = input.nextInt();
+         System.out.println();
+
+         return option;
+     }
     public static void main(String[] args) {
         DoublyLinkedList dbl = new DoublyLinkedList();
         Product prod = new Product("tomate", 8);
         Product prod2 = new Product("abacate", 2);
+        Product prod3 = new Product("jiló", 7);
 
         dbl.add(prod);
         dbl.add(prod2);
+        dbl.add(prod3);
         Sorter.sortDoublyLinkedList(dbl);
         System.out.println(dbl);
     }
